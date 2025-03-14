1e039bf1e935321de6b81f27c982fb0e6bf8b7f2
krasmik <krasmik@qamadness.com>
2025-03-14 16:27:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+// const chalk = require('chalk');
+
+console.log(chalk.blue('Hello World'));
+console.log(chalk.red('Hello World'));
---
+++ variables.js
@@ -0,0 +1,35 @@
+// // console.log(email);
+
+// // var age = 15;
+// let email = 'krasmik@qamadness.com';
+
+// const firstName = 'John';
+
+// // let userEmailVerified
+
+// email = 20;
+
+// // console.log(age);
+// console.log(email);
+// console.log(firstName);
+
+
+let userAge = 20;
+let userName = 'Ivan';
+// let welcomeMessage = `Hi, my name is ${userName}, I am ${userAge}`;
+
+
+// console.log('Hi, my name is ' + userName);
+// console.log("I am " + userAge);
+
+// console.log('Hi, my name is ' + userName + ', I am ' + userAge);
+// console.log(welcomeMessage);
+
+// console.log("abc" / 4);
+
+
+let isUserLogged = true;
+
+console.log(typeof isUserLogged);
+console.log(typeof userAge);
+console.log(typeof userName);
